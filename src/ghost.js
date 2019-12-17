7f0deb3262140b9e48777f673ba4fda073be7cc4
JohnCharamonde <john.charamonde@gmail.com>
2019-12-17 15:56:21 -0500

    Ghost and Pacman working. Animation progress in jQuery.

--- src/ghost.js
+++ src/ghost.js
@@ -1,6 +1,7 @@
 var Ghost = function (top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('ghost');
+  console.log('before', top,left);
   // this.left = 100%;
   // this.slide = $(this.$node).animate({ "left": "-=10px" }, "slow" )
   // this.move();
@@ -11,7 +12,8 @@ Ghost.prototype.constructor = Ghost;
 
 Ghost.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.$node.animate({ 'left': '+=100px' }, 'linear' );
+  this.$node.animate({ 'left': `${this.left += 100}` }, 'linear' );
+  console.log(this.left);
   // if(this.$node.left >= 2000px) {
   //   this.$node.remove();
   // }
